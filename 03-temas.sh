c0b381cfc6b7a07e278b93bff71044fcf9beff4d
Rafael Y. De Vidis <rafael.ydv@gmail.com>
2020-08-18 02:39:12 -0400

    Removido pacote dos wallpapers do Arch Linux.

--- 03-temas.sh
+++ 03-temas.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ##Pacotes de temas
-pacman -S archlinux-wallpaper \
-	  adapta-gtk-theme \
+pacman -S adapta-gtk-theme \
 	  papirus-icon-theme \
 	  deepin-icon-theme \
 	  arc-gtk-theme \
